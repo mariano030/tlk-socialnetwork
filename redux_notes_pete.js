227c18e9d00d8fe5c5ecf18eab5dba0c32347b58
mariano030 <marian.heidbreder@gmail.com>
2020-10-08 14:55:30 +0200

    backup

---
+++ redux_notes_pete.js
@@ -0,0 +1,20 @@
+//////// REDUX ////////
+
+// nerver really change the object, always copy it
+// history is available with older versions of the state obj
+
+// actions have two keys
+//  type & payload
+
+//only pure functions to be done in reducer
+// (= functions that do not change anything outside of the fn())
+
+// shallow cloning
+//var user2 = {...user}
+// if you ...clone an object the objects one level
+// down get connected to the new copy and are still the SAME object
+
+// possilbe copy hack: use json stringify and rejobectify bam full copy??!
+
+// alwasy deep clone!
+// optional chaining ?.
